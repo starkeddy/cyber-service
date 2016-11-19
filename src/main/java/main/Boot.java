263f78a0a47dd9f0768a3c7c55b1ed67236a41d4
starkeddy <geraleddy.stark@gmail.com>
2016-11-19 12:00:24 +0300

    first

---
+++ src/main/java/main/Boot.java
@@ -0,0 +1,15 @@
+package main;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
+/**
+ * Created by eddy on 19/11/2016.
+ */
+
+public class Boot {
+    public static void main(String[]args){
+        System.out.println("Mety");
+
+    }
+}
